85aae98036484970df7ff3567058016457ff1d1a
Jovian Experiments <jovian-experiments@>
2022-08-10 19:11:28 +0000

    steam-jupiter-oobe-1.0.0.74-2.19
    Archive sha256sum: 5842c0189bec578b803d89d51229d6f752e74f7dbbe23fdbc3b2f264325ee1b0 jupiter-3.5/steam-jupiter-oobe-1.0.0.74-2.19.src.tar.gz

---
+++ steam-jupiter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+STEAM_LINKS="$HOME"/.steam
+STEAM_DIR="$HOME"/.local/share/Steam
+
+(
+  # On OOBE images we want to always start with a fresh steam per boot as we lack the proper steam overlay/repair code
+  rm -rf --one-file-system "$STEAM_LINKS" "$STEAM_DIR"
+
+  # Similarly, on OOBE in this mode, always inhibit the bootstrapper
+  # Also see steam-jupiter-stable's variant of this script, which looks for this marker
+  mkdir -pv "$STEAM_DIR"
+  echo -e "# OOBE Inhibit\nBootStrapperInhibitAll = enable" > "$STEAM_DIR"/Steam.cfg
+)
+exec /usr/lib/steam/steam "$@"
